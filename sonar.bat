f50963518c63115affbdc57c5b3b896995c495c8
Mario Ruano <mrf1989@hotmail.com>
2020-04-08 14:19:20 +0200

    Update sonar.bat

--- sonar.bat
+++ sonar.bat
@@ -1,1 +1,1 @@
-mvn sonar:sonar -Dsonar.projectKey=PSG2-1920-G7-73-L2:spring-petclinic -Dsonar.host.url=http://oitilo.us.es:9000 -Dsonar.login=9ee3f4221f8a8f03c3a7577517cca6d31c63f846
+mvn sonar:sonar -Dsonar.projectKey=PSG2-1920-G7-73-L3:spring-petclinic -Dsonar.host.url=http://oitilo.us.es:9000 -Dsonar.login=9fa36bc63e74c427c59862fb5ca6b5d3be21cab6
